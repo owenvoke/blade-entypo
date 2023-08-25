cd4321e097913364c71ad1bc37dabdb7ce10cf79
Owen Voke <development@voke.dev>
2023-08-25 10:18:55 +0100

    feat: update to use Safe PHP

--- config/generation.php
+++ config/generation.php
@@ -1,10 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+use function Safe\file_get_contents;
+use function Safe\file_put_contents;
+
 // Uses Icons from:
 // https://github.com/adamwathan/entypo-optimized/tree/master/dist/icons
 
 $svgNormalization = static function (string $tempFilepath, array $iconSet) {
-    /** @var string $svgContent */
     $svgContent = file_get_contents($tempFilepath);
     $svgContent = str_replace('<svg ', '<svg fill="currentColor" ', $svgContent);
     file_put_contents($tempFilepath, $svgContent);
