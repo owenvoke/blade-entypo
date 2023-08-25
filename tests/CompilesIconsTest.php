bf921e545aad65c85029594cf975fcb9ab6e37f2
Owen Voke <development@voke.dev>
2023-08-25 10:20:07 +0100

    chore: update to use Pest built-in snapshots

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -4,24 +4,22 @@ declare(strict_types=1);
 
 use OwenVoke\BladeEntypo\Tests\TestCase;
 
-use function Spatie\Snapshots\assertMatchesSnapshot;
-
 uses(TestCase::class);
 
 it('compiles a single anonymous component', function () {
     $result = svg('entypo-plus')->toHtml();
 
-    assertMatchesSnapshot($result);
+    expect($result)->toMatchSnapshot();
 });
 
 it('can add classes to icons', function () {
     $result = svg('entypo-plus', 'w-6 h-6 text-gray-500')->toHtml();
 
-    assertMatchesSnapshot($result);
+    expect($result)->toMatchSnapshot();
 });
 
 it('can add styles to icons', function () {
     $result = svg('entypo-plus', ['style' => 'color: #555'])->toHtml();
 
-    assertMatchesSnapshot($result);
+    expect($result)->toMatchSnapshot();
 });
