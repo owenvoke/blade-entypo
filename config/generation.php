6722fb7e1de633550880fa37d7469e4634d673f6
Owen Voke <development@voke.dev>
2025-01-06 14:36:38 +0000

    chore(deps): remove Safe dependency

--- config/generation.php
+++ config/generation.php
@@ -2,16 +2,24 @@
 
 declare(strict_types=1);
 
-use function Safe\file_get_contents;
-use function Safe\file_put_contents;
+use RuntimeException;
 
 // Uses Icons from:
 // https://github.com/adamwathan/entypo-optimized/tree/master/dist/icons
 
 $svgNormalization = static function (string $tempFilepath, array $iconSet) {
     $svgContent = file_get_contents($tempFilepath);
+
+    if ($svgContent === false) {
+        throw new RuntimeException('Unable to read SVG file.');
+    }
+
     $svgContent = str_replace('<svg ', '<svg fill="currentColor" ', $svgContent);
-    file_put_contents($tempFilepath, $svgContent);
+    $result = file_put_contents($tempFilepath, $svgContent);
+
+    if ($result === false) {
+        throw new RuntimeException('Unable to write SVG file.');
+    }
 };
 
 return [
